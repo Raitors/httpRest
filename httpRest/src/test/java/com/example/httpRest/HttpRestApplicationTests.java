d6ca2fa2137e59fffc1f31e4122b2414ad687eba
Shashkin <artyom.sh.s@yandex.ru>
2023-08-08 20:56:43 +0300

    init

---
+++ httpRest/src/test/java/com/example/httpRest/HttpRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.httpRest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HttpRestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
